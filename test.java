ef5af9459ed23bc1d613a76c8d86886bf93bf5f7
hajin428 <amm_321@naver.com>
2024-06-20 14:04:03 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,65 +1,64 @@
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class test {
     public static void main(String[] args) {
+
+        // 중복되지 않는 숫자 3개 뽑기
         int[] computer = new int[3];
         int[] user = new int[3];
+
         Random computerNum = new Random();
 
         System.out.println("컴퓨터가 숫자를 생성하였습니다. 숫자를 입력하세요.");
 
-        // 중복되지 않는 숫자 3개 뽑기
-        for (int i = 0; i < computer.length; i++) {
-            boolean unique;
-            do {
-                unique = true;
+        //중복 확인하기
+        do {
+            for (int i = 0; i < computer.length; i++) {
                 computer[i] = computerNum.nextInt(10);
-                for (int j = 0; j < i; j++) {
-                    if (computer[i] == computer[j]) {
-                        unique = false;
-                        break;
-                    }
-                }
-            } while (!unique);
-        }
+            }
+        } while (computer[0] == computer[1] || computer[0] == computer[2] || computer[1] == computer[2]);
 
         Scanner scanner = new Scanner(System.in);
-        int attempts = 0;
 
+        int attempts = 0;
         while (true) {
             attempts++;
 
-            System.out.print(attempts + "번째 시도: ");
-            for (int i = 0; i < user.length; i++) {
-                user[i] = scanner.nextInt();
-            }
+            // 사용자로부터 숫자 입력 받기
+            for (int number = 1; ; number++) {
+                System.out.print(attempts + "번째 시도: ");
+                for (int i = 0; i < user.length; i++) {
+                    user[i] = scanner.nextInt();
+                }
 
-            int strikes = 0;
-            int balls = 0;
+                //스트라이크, 볼
+                int strikes = 0;
+                int balls = 0;
 
-            // 스트라이크와 볼 계산
-            for (int i = 0; i < computer.length; i++) {
-                for (int j = 0; j < user.length; j++) {
-                    if (computer[i] == user[j]) {
-                        if (i == j) {
-                            strikes++;
-                        } else {
-                            balls++;
+                //배열의 길이만큼 컴퓨터와 유저 숫자 비교하기
+                for (int i = 0; i < computer.length; i++) {
+                    for (int j = 0; j < computer.length; j++) {
+                        if (computer[i] == user[i]) {
+                            if (i == j) {
+                                strikes++;
+                            } else {
+                                balls++;
+                            }
+                            //결과
+                            System.out.println(balls + "B" + strikes + "S");
+
+                            //게임 종료
+                            if(strikes == 3){
+                                System.out.println(strikes + "S");
+                                System.out.println( attempts + "번 만에 게임을 종료합니다.");
+                                break;
+                            }
                         }
                     }
                 }
             }
-
-            System.out.println(balls + "B " + strikes + "S");
-
-            // 게임 종료 조건
-            if (strikes == 3) {
-                System.out.println(strikes + "S");
-                System.out.println(attempts + "번 만에 게임을 종료합니다.");
-                break;
-            }
         }
-
     }
 }
